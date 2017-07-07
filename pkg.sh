5ab428fcf131af1024528e74d3d024a173004fce
CuriousFu <2269930627@qq.com>
2017-07-07 15:05:31 +0800

    modified:   config 	cmus.sh

--- pkg.sh
+++ pkg.sh
@@ -7,13 +7,15 @@ check=$((pac + aur))
 while [[ "$check" != "0" ]]
 do
     echo "$pac %{F#ff00b7}%{F-} $aur"
-    sleep 2s
+    sleep 1m
     pacaur -Syu --noconfirm --noedit
     if [ $? -eq 0 ]
     then
         sleep 3s
    # i3 restart 
    sh ~/.config/polybar/launch.sh &
+   else
+       pacaur -Sc --noconfirm
    fi
 #else
    # echo "$pac %{F#ff00b7}%{F-} $aur">/dev/null 2>&1 
